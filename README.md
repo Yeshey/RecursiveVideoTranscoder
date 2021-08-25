2d1e163f48b25fec492396dc95189587d3b99234
Yeshey <yesheysangpo@hotmail.com>
2021-08-25 17:52:55 +0100

    adding interrupt through double CTRL + C input

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ in terminal `cd` to the desiered directory and run
 ```curl -s https://raw.githubusercontent.com/Yeshey/RecursiveVideoTranscoder/main/RecursiveVideoTranscoder.sh | bash```
 
 # Know Issues:
-CTRL+C SHOULDN'T BE USED!, as it deletes the original file and leaves the unfinished product of ffmpeg, then keeps going for other files, there is no way to cancel the process currently & deleted files are not recoverable as they are not sent to the recycle bin
+CTRL+C Should be used carefully! To cancell, 2 CTRL+Cs should be inputed in the same second, one for the ffmpeg process and one for the script, 1 CTRL + C will result in loss of data & deleted files are not recoverable
 
 there is only one transcoding command that keeps the video as is, just codificated in a more efficient way. No prompts are made as to how the user wants the process to happen.
 
--- RecursiveVideoTranscoder.sh
+++ RecursiveVideoTranscoder.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+(
+    trap printout SIGINT
+    printout() {
+        echo "Exiting..."
+        exit
+    }
 echo "This script will recursively search for any videos and transcode them in place to make'em smaller"
 # echo "do you wish to append _f to transcoded videos?, videos that end with _f won't be transcoded"
 # echo "Do you wish to change frame rate?"
@@ -13,7 +20,9 @@ find . \( -iname '*.kvm' -o -iname '*avi' -o -iname '*mp4' -o -iname '*flv' -o -
                 # < /dev/null to prevent from reading standard input (Strange errors when using ffmpeg in a loop)
                 # -max_muxing_queue_size 1024 needed for certain situations (FFMPEG: Too many packets buffered)
                 < /dev/null ffmpeg -i "$old" -vcodec libx265 -crf 28 -max_muxing_queue_size 1024 "${file%.*}_f.mp4"
+                sleep 1
                 rm "$old"
                 echo "file $file transcoded..."
             fi
         done
+)
