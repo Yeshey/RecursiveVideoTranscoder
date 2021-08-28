aff7578b51b6bda51086e333f3f6f80f4e97c81d
Yeshey <yesheysangpo@hotmail.com>
2021-08-29 00:36:54 +0100

    will exit with 1 CTRL+C now

--- RecursiveVideoTranscoder.sh
+++ RecursiveVideoTranscoder.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# https://unix.stackexchange.com/questions/230421/unable-to-stop-a-bash-script-with-ctrlc
 (
-    trap printout SIGINT
+    trap "printout" "$old" SIGINT
     printout() {
         echo "Exiting..."
+        echo "$1"
         exit
     }
 echo "This script will recursively search for any videos and transcode them in place to make'em smaller"
@@ -12,16 +14,15 @@ echo "This script will recursively search for any videos and transcode them in p
 find . \( -iname '*.kvm' -o -iname '*avi' -o -iname '*mp4' -o -iname '*flv' -o -iname '*ogg' -o -iname '*mov' -o -iname '*asf' -o -iname '*mkv' \) -print |
     while IFS= read file    # IFS= prevents "read" stripping whitespace
         do
-            #If it hasn't been transcoded yet (How do I compare two string variables in an 'if' statement in Bash?)
             if [[ "$file" != *"_f."* ]]
             then
                 old="${file%.*}_old_f.${file##*.}"
                 mv "$file" "$old"
                 # < /dev/null to prevent from reading standard input (Strange errors when using ffmpeg in a loop)
                 # -max_muxing_queue_size 1024 needed for certain situations (FFMPEG: Too many packets buffered)
-                < /dev/null ffmpeg -i "$old" -vcodec libx265 -crf 28 -max_muxing_queue_size 1024 "${file%.*}_f.mp4"
-                sleep 1
-                rm "$old"
+                < /dev/null ffmpeg -i "$old" -vcodec libx265 -crf 28 -max_muxing_queue_size 1024 "${file%.*}_f.mp4" || exit
+
+                 rm "$old"
                 echo "file $file transcoded..."
             fi
         done
